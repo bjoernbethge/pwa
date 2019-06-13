605bd2b5d7d198339d833e95a8d9301cdfe21cd8
bjoernbethge <bjoern.bethge@gmail.com>
2019-06-13 10:52:36 +0200

    Demo PWA

---
+++ precache-manifest.bf474dd3ac6148486d9c88b4e11cd741.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e2f7329f2cb7a26f365a392f9799846",
+    "url": "/logo-192.png"
+  },
+  {
+    "revision": "b3c687f18f58015e032d5d4b5c5c33e8",
+    "url": "/draco/gltf/draco_encoder.js"
+  },
+  {
+    "revision": "0c04a6d0159e22f41295c60c0e772507",
+    "url": "/logo.png"
+  },
+  {
+    "revision": "9729bf9ab292cb19eb51",
+    "url": "/js/chunk-vendors.b45f62c7.js"
+  },
+  {
+    "revision": "93feb000eb9cd20e50037e504fecb5e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b39024a64cc3e68378876128ce4bdac9",
+    "url": "/draco/gltf/draco_wasm_wrapper.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f2ddf0f3d58623134de68469ca8f2d82",
+    "url": "/sw.js"
+  },
+  {
+    "revision": "d4cbe40bc910fa8e5a3b614023b9d0c8",
+    "url": "/logo-128.png"
+  },
+  {
+    "revision": "eaf69605c8af67f639c7",
+    "url": "/js/app.1e2fa71e.js"
+  },
+  {
+    "revision": "3b885e89b358419ffb836b5406c83732",
+    "url": "/draco/gltf/draco_decoder.wasm"
+  },
+  {
+    "revision": "2df3c1e89d9fa13d5e7d034aef753d91",
+    "url": "/draco/gltf/draco_decoder.js"
+  },
+  {
+    "revision": "1d2c198266da1a85e66eac49cdd36dfb",
+    "url": "/draco/draco_wasm_wrapper.js"
+  },
+  {
+    "revision": "b0686b76852d262a692859bd67e3a903",
+    "url": "/draco/draco_decoder.wasm"
+  },
+  {
+    "revision": "2787db01537cdfbdbea455eaaf851395",
+    "url": "/draco/draco_decoder.js"
+  },
+  {
+    "revision": "195bdce75c64069cf764fe510727b2c8",
+    "url": "/draco/README.md"
+  },
+  {
+    "revision": "9729bf9ab292cb19eb51",
+    "url": "/css/chunk-vendors.ff3c5db6.css"
+  },
+  {
+    "revision": "eaf69605c8af67f639c7",
+    "url": "/css/app.f02ba254.css"
+  }
+];
